96615e2a67ac05a33095fde58f63c6d26d319a14
조송하 <102511282+Song-haJo@users.noreply.github.com>
2022-12-17 22:41:33 +0900

    Create Search.aspx.cs

---
+++ Search.aspx.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Search : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void logInLnkBtn_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void signUpLnkBtn_Click(object sender, EventArgs e)
+    {
+
+    }
+}
