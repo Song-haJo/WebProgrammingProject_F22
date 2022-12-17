88ff2a045ca1e5d7580f9ace72cdfa3bfd477da1
EunKyeong Cho <74223597+Zyayou@users.noreply.github.com>
2022-12-18 01:16:21 +0900

    Update Search.aspx.cs

--- Search.aspx.cs
+++ Search.aspx.cs
@@ -9,6 +9,10 @@ public partial class Search : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack){
+            regionTB.Text = Request.QueryString["region"];
+            peopleDDL.Text = Request.QueryString["people"];
+        }
 
     }
 
