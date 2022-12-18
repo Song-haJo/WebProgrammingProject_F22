af82bee6a0e07ab80d18d75c469e0a28a18d724d
EunKyeong Cho <74223597+Zyayou@users.noreply.github.com>
2022-12-18 23:42:02 +0900

    Update Search.aspx.cs

--- Search.aspx.cs
+++ Search.aspx.cs
@@ -44,50 +44,149 @@ public partial class Search : System.Web.UI.Page
 
     protected void logInLnkBtn_Click(object sender, EventArgs e)
     {
-
+        Response.Redirect(string.Format("Login.aspx?"));
     }
 
     protected void signUpLnkBtn_Click(object sender, EventArgs e)
     {
-
+        Response.Redirect(string.Format("Singup.aspx?"));
     }
     
-    protected void optionCBL_SelectedIndexChanged(object sender, EventArgs e)
-    {
-            
-        string selectSQL = "SELECT Accommodation_Name.A_Name, Accommodation_Name.A_Address, Accommodation_Info.Room_Numer, " +
-            "Accommodation_Info.Price, Accommodation_Info.Room_type, Accommodation_Name.A_Option FROM Accommodation_Name " +
-            "INNER JOIN Accommodation_Info ON Accommodation_Name.A_Address = Accommodation_Info.A_Address";
-        selectSQL += " WHERE (Accommodation_Name.A_Address LIKE '%" + Request.QueryString["region"];
-        selectSQL += "%' ";
-        selectSQL += "OR A_Name LIKE '%" + Request.QueryString["region"];
-        selectSQL += "%') ";
-        selectSQL += "AND Room_Numer >= " + Request.QueryString["people"];
-            
-        string str = "";
-        foreach (ListItem item in optionCBL.Items)
+     protected void optionCBL_SelectedIndexChanged(object sender, EventArgs e)
         {
+            string selectSQL ="SELECT Accommodation_Name.A_Name, Accommodation_Name.A_Address, Accommodation_Info.Room_Numer, " +
+                "Accommodation_Info.Price, Accommodation_Info.Room_type, Accommodation_Name.A_Option FROM Accommodation_Name " +
+                "INNER JOIN Accommodation_Info ON Accommodation_Name.A_Address = Accommodation_Info.A_Address";
+            selectSQL += " WHERE (Accommodation_Name.A_Address LIKE '%" + Request.QueryString["region"];
+            selectSQL += "%' ";
+            selectSQL += "OR A_Name LIKE '%" + Request.QueryString["region"];
+            selectSQL += "%') ";
+            selectSQL += "AND Room_Numer >= " + Request.QueryString["people"];
             
-            if (item.Selected)
+
+            string str = "";
+            foreach (ListItem item in optionCBL.Items)
             {
-                if (str == "")
+                if (item.Selected)
                 {
-                    str += "'" + item.Value.ToString() + "'";
-                    selectSQL += " AND A_Option IN ('" + item.Value.ToString() +"'";
-                }
-                else {
-                    str += "," + "'" + item.Value.ToString() + "'";
-                    selectSQL += ",'" + item.Value.ToString() + "'";
-                }
-                  
+                    if (str == "")
+                    {
+                        str += "'" + item.Value.ToString() + "'";
+                        selectSQL += " AND A_Option IN ('" + item.Value.ToString() +"'";
+                    }
+                    else {
+                        str += "," + "'" + item.Value.ToString() + "'";
+                        selectSQL += ",'" + item.Value.ToString() + "'";
+                    }  
+                }   
             }
-             
-                
+
+            if(optionCBL.SelectedItem != null)
+            {
+                selectSQL += ")";
+            }
+
+            if(MinPriceTB.Text != "")
+            {
+                selectSQL += " AND Price >= " + MinPriceTB.Text;
+            }
+            if (MaxPriceTB.Text != "")
+            {
+                selectSQL += " AND Price <= " + MaxPriceTB.Text;
+            }
+            SqlDataSource1.SelectCommand = selectSQL;
         }
-        if(optionCBL.SelectedItem != null)
+
+        protected void MinPriceTB_TextChanged(object sender, EventArgs e)
         {
-            selectSQL += ")";
+            string selectSQL = "SELECT Accommodation_Name.A_Name, Accommodation_Name.A_Address, Accommodation_Info.Room_Numer, " +
+                "Accommodation_Info.Price, Accommodation_Info.Room_type, Accommodation_Name.A_Option FROM Accommodation_Name " +
+                "INNER JOIN Accommodation_Info ON Accommodation_Name.A_Address = Accommodation_Info.A_Address";
+            selectSQL += " WHERE (Accommodation_Name.A_Address LIKE '%" + Request.QueryString["region"];
+            selectSQL += "%' ";
+            selectSQL += "OR A_Name LIKE '%" + Request.QueryString["region"];
+            selectSQL += "%') ";
+            selectSQL += "AND Room_Numer >= " + Request.QueryString["people"];
+
+
+            string str = "";
+            foreach (ListItem item in optionCBL.Items)
+            {
+                if (item.Selected)
+                {
+                    if (str == "")
+                    {
+                        str += "'" + item.Value.ToString() + "'";
+                        selectSQL += " AND A_Option IN ('" + item.Value.ToString() + "'";
+                    }
+                    else
+                    {
+                        str += "," + "'" + item.Value.ToString() + "'";
+                        selectSQL += ",'" + item.Value.ToString() + "'";
+                    }
+                }
+            }
+
+            if (optionCBL.SelectedItem != null)
+            {
+                selectSQL += ")";
+            }
+
+            if (MinPriceTB.Text != "")
+            {
+                selectSQL += " AND Price >= " + MinPriceTB.Text;
+            }
+            if (MaxPriceTB.Text != "")
+            {
+                selectSQL += " AND Price <= " + MaxPriceTB.Text;
+            }
+            SqlDataSource1.SelectCommand = selectSQL;
         }
-        SqlDataSource1.SelectCommand = selectSQL;
+
+        protected void MaxPriceTB_TextChanged(object sender, EventArgs e)
+        {
+            string selectSQL = "SELECT Accommodation_Name.A_Name, Accommodation_Name.A_Address, Accommodation_Info.Room_Numer, " +
+                "Accommodation_Info.Price, Accommodation_Info.Room_type, Accommodation_Name.A_Option FROM Accommodation_Name " +
+                "INNER JOIN Accommodation_Info ON Accommodation_Name.A_Address = Accommodation_Info.A_Address";
+            selectSQL += " WHERE (Accommodation_Name.A_Address LIKE '%" + Request.QueryString["region"];
+            selectSQL += "%' ";
+            selectSQL += "OR A_Name LIKE '%" + Request.QueryString["region"];
+            selectSQL += "%') ";
+            selectSQL += "AND Room_Numer >= " + Request.QueryString["people"];
+
+
+            string str = "";
+            foreach (ListItem item in optionCBL.Items)
+            {
+                if (item.Selected)
+                {
+                    if (str == "")
+                    {
+                        str += "'" + item.Value.ToString() + "'";
+                        selectSQL += " AND A_Option IN ('" + item.Value.ToString() + "'";
+                    }
+                    else
+                    {
+                        str += "," + "'" + item.Value.ToString() + "'";
+                        selectSQL += ",'" + item.Value.ToString() + "'";
+                    }
+                }
+            }
+
+            if (optionCBL.SelectedItem != null)
+            {
+                selectSQL += ")";
+            }
+
+            if (MinPriceTB.Text != "")
+            {
+                selectSQL += " AND Price >= " + MinPriceTB.Text;
+            }
+            if (MaxPriceTB.Text != "")
+            {
+                selectSQL += " AND Price <= " + MaxPriceTB.Text;
+            }
+            SqlDataSource1.SelectCommand = selectSQL;
+        }
     }
 }
