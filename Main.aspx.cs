d6c3b649047b9a61185c6b936e4e5685766b7481
EunKyeong Cho <74223597+Zyayou@users.noreply.github.com>
2022-12-19 05:37:18 +0900

    Update Main.aspx.cs

--- Main.aspx.cs
+++ Main.aspx.cs
@@ -5,33 +5,49 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using System.Web.Security;
+
 public partial class Main : System.Web.UI.Page
 {
-    protected void Page_Load(object sender, EventArgs e)
+    public partial class Main : System.Web.UI.Page
     {
+        protected void Page_Load(object sender, EventArgs e)
+        {
 
-    }
+        }
 
-    protected void logInLnkBtn_Click(object sender, EventArgs e)
-    {
+        protected void logInLnkBtn_Click(object sender, EventArgs e)
+        {
+            Response.Redirect(string.Format("Login.aspx?"));
+        }
 
-    }
+        protected void signUpLnkBtn_Click(object sender, EventArgs e)
+        {
+            Response.Redirect(string.Format("Singup.aspx?"));
+        }
 
-    protected void signUpLnkBtn_Click(object sender, EventArgs e)
-    {
+        protected void searchBtn_Click(object sender, EventArgs e)
+        {
+            string queryString = "region=" + region.Text;
+            queryString += "&";
+            queryString += "people=" + people.Text;
+            queryString += "&";
+            queryString += "dateFrom=" + CalendarFrom.SelectedDate.ToString("yyyy-MM-dd");
+            queryString += "&";
+            queryString += "dateTo=" + CalendarTo.SelectedDate.Date.ToString("yyyy-MM-dd");
+            Response.Redirect(string.Format("Search.aspx?"+ queryString));
+        }
 
-    }
-    
-    protected void searchBtn_Click(object sender, EventArgs e)
-    {//날짜 전송 추가
-        string queryString = "region=" + region.Text;
-        queryString += "&";
-        queryString += "people=" + people.Text;
-        queryString += "&";
-        queryString += "dateFrom=" + CalendarFrom.SelectedDate.ToString("yyyy-MM-dd");
-        queryString += "&";
-        queryString += "dateTo=" + CalendarTo.SelectedDate.Date.ToString("yyyy-MM-dd");
-        Response.Redirect(string.Format("Search.aspx?"+ queryString));
+        protected void logOutLnkBtn_Click(object sender, EventArgs e)
+        {
+            FormsAuthentication.SignOut();
+            Response.Redirect(string.Format("Main.aspx?"));
+        }
+
+        protected void Mypage_Click(object sender, EventArgs e)
+        {
+            Response.Redirect(string.Format("My.aspx?"));
+        }
     }
     
 }
