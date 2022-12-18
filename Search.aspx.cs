1bb002b71c8a4d31f209b03560156c1e425d7040
EunKyeong Cho <74223597+Zyayou@users.noreply.github.com>
2022-12-18 21:46:06 +0900

    Update Search.aspx.cs

--- Search.aspx.cs
+++ Search.aspx.cs
@@ -12,7 +12,33 @@ public partial class Search : System.Web.UI.Page
         if (!IsPostBack){
             regionTB.Text = Request.QueryString["region"];
             peopleDDL.Text = Request.QueryString["people"];
+            fromD.Value = Request.QueryString["dateFrom"];
+            toD.Value = Request.QueryString["dateTo"];
+                
+
+            string selectSQL = "SELECT Accommodation_Name.A_Name, Accommodation_Name.A_Address, Accommodation_Info.Room_Numer, " +
+            "Accommodation_Info.Price, Accommodation_Info.Room_type, Accommodation_Name.A_Option FROM Accommodation_Name " +
+            "INNER JOIN Accommodation_Info ON Accommodation_Name.A_Address = Accommodation_Info.A_Address";
+            selectSQL += " WHERE (Accommodation_Name.A_Address LIKE '%" + regionTB.Text;
+            selectSQL += "%' ";
+            selectSQL += "OR A_Name LIKE '%" + regionTB.Text;
+            selectSQL += "%') ";
+            selectSQL += "AND Room_Numer >= " + peopleDDL.Text;
+            SqlDataSource1.SelectCommand = selectSQL;
         }
+
+    }
+    
+    protected void Button1_Click(object sender, EventArgs e)
+    {    
+        string queryString = "region=" + regionTB.Text;
+        queryString += "&";
+        queryString += "people=" + peopleDDL.Text;
+        queryString += "&";
+        queryString += "dateFrom=" + fromD.Value;
+        queryString += "&";
+        queryString += "dateTo=" + toD.Value;
+        Response.Redirect(string.Format("Search.aspx?" + queryString));
 
     }
 
@@ -24,5 +50,44 @@ public partial class Search : System.Web.UI.Page
     protected void signUpLnkBtn_Click(object sender, EventArgs e)
     {
 
+    }
+    
+    protected void optionCBL_SelectedIndexChanged(object sender, EventArgs e)
+    {
+            
+        string selectSQL = "SELECT Accommodation_Name.A_Name, Accommodation_Name.A_Address, Accommodation_Info.Room_Numer, " +
+            "Accommodation_Info.Price, Accommodation_Info.Room_type, Accommodation_Name.A_Option FROM Accommodation_Name " +
+            "INNER JOIN Accommodation_Info ON Accommodation_Name.A_Address = Accommodation_Info.A_Address";
+        selectSQL += " WHERE (Accommodation_Name.A_Address LIKE '%" + Request.QueryString["region"];
+        selectSQL += "%' ";
+        selectSQL += "OR A_Name LIKE '%" + Request.QueryString["region"];
+        selectSQL += "%') ";
+        selectSQL += "AND Room_Numer >= " + Request.QueryString["people"];
+            
+        string str = "";
+        foreach (ListItem item in optionCBL.Items)
+        {
+            
+            if (item.Selected)
+            {
+                if (str == "")
+                {
+                    str += "'" + item.Value.ToString() + "'";
+                    selectSQL += " AND A_Option IN ('" + item.Value.ToString() +"'";
+                }
+                else {
+                    str += "," + "'" + item.Value.ToString() + "'";
+                    selectSQL += ",'" + item.Value.ToString() + "'";
+                }
+                  
+            }
+             
+                
+        }
+        if(optionCBL.SelectedItem != null)
+        {
+            selectSQL += ")";
+        }
+        SqlDataSource1.SelectCommand = selectSQL;
     }
 }
